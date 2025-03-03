4d0f97578b302f75f0f4d3644a6f36d73a53b19e
ahuseyn <huseyn.aghayev@wpengine.com>
2025-03-03 13:31:29 +0100

    chore: setup Apollo Client

--- examples/nextjs-pages-router/apollo-client-data-fetch/src/pages/_app.js
+++ examples/nextjs-pages-router/apollo-client-data-fetch/src/pages/_app.js
@@ -1,5 +1,17 @@
 import "@/styles/globals.css";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+
+const WORDPRESS_URL = process.env.NEXT_PUBLIC_WORDPRESS_URL;
+
+const client = new ApolloClient({
+  uri: WORDPRESS_URL + "/graphql",
+  cache: new InMemoryCache(),
+});
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  );
 }
