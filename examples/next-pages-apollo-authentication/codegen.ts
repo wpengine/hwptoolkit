08d07056179e586aeb2551d94ce9deb8c7af848e
Theo <328805+theodesp@users.noreply.github.com>
2025-03-04 12:19:00 +0000

    fix: update codegen.ts

--- examples/next-pages-apollo-authentication/codegen.ts
+++ examples/next-pages-apollo-authentication/codegen.ts
@@ -1,16 +1,22 @@
-import type { CodegenConfig } from '@graphql-codegen/cli';
- 
- const config: CodegenConfig = {
-   schema: 'http://myexample.local/graphql',
-   documents: ['src/**\/*.jsx'],
-   generates: {
-     'possibleTypes.json': {
-       plugins: ['fragment-matcher'],
-       config: {
-         module: 'commonjs',
-         apolloClientVersion: 3,
-       },
-     },
-   },
- };
- export default config;
+import type { CodegenConfig } from "@graphql-codegen/cli";
+import dotenv from "dotenv";
+dotenv.config();
+
+const baseUrl =
+  process.env.NEXT_PUBLIC_WORDPRESS_URL || "https://your-wordpress-site.com";
+const graphqlPath = process.env.NEXT_PUBLIC_GRAPHQL_PATH || "/graphql";
+
+const config: CodegenConfig = {
+  schema: `${baseUrl}${graphqlPath}`,
+  documents: ["src/**/*.jsx"],
+  generates: {
+    "possibleTypes.json": {
+      plugins: ["fragment-matcher"],
+      config: {
+        module: "commonjs",
+        apolloClientVersion: 3,
+      },
+    },
+  },
+};
+export default config;
