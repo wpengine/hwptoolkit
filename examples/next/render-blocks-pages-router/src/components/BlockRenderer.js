74b5fa56b909dd3423c23ac0f37da19d563b2271
ahuseyn <huseyn.aghayev@wpengine.com>
2025-03-19 18:01:01 +0100

    feat: add BlockRenderer and DefaultBlock

---
+++ examples/next/render-blocks-pages-router/src/components/BlockRenderer.js
@@ -0,0 +1,35 @@
+import { blocks as blockComponents } from "@/components/blocks";
+import DefaultBlock from "./DefaultBlock";
+
+export function BlockRenderer({ blocks, defaultBlock, customParser }) {
+  // If blocks is not an array, don't render anything
+  if (!Array.isArray(blocks)) {
+    return null;
+  }
+
+  return blocks.map((block) => {
+    const { id, children, __typename, attributes } = block ?? {};
+
+    // If there is no __typename, we can't determine which block to render
+    if (!__typename) {
+      console.error("Block is missing __typename. Please add it to your query.", block);
+
+      return null;
+    }
+
+    const BlockComponent = blockComponents[__typename];
+
+    // If there is no BlockComponent, render the default block
+    if (!BlockComponent) {
+      const CustomDefaultBlock = defaultBlock;
+
+      return defaultBlock ? <CustomDefaultBlock key={id} /> : <DefaultBlock key={id} />;
+    }
+
+    return (
+      <BlockComponent key={id} attributes={attributes}>
+        {children && <BlockRenderer blocks={children} defaultBlock={defaultBlock} customParser={customParser} />}
+      </BlockComponent>
+    );
+  });
+}
