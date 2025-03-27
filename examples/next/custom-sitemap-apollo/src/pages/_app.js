5587589b999938d3fe1913e61b9b290e89192c41
ahuseyn <huseyn.aghayev@wpengine.com>
2025-03-27 17:17:28 +0100

    chore: install and setup Apollo Client

--- examples/next/custom-sitemap-apollo/src/pages/_app.js
+++ examples/next/custom-sitemap-apollo/src/pages/_app.js
@@ -1,5 +1,12 @@
 import "@/styles/globals.css";
+import { client } from "@/lib/client";
+import { ApolloProvider } from "@apollo/client";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    // ApolloProvider makes the Apollo Client available to the rest of the app
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  );
 }
