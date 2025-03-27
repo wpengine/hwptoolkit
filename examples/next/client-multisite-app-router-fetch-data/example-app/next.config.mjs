4ab1efe8d71d71b9c774af4d68ba891cc9a96c49
Colin Murphy <colinmurphy@users.noreply.github.com>
2025-03-27 18:52:38 +0000

    Setup layout for app. Fixed metadata for previous client example.

--- examples/next/client-multisite-app-router-fetch-data/example-app/next.config.mjs
+++ examples/next/client-multisite-app-router-fetch-data/example-app/next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    reactStrictMode: true,
+    images: {
+      // Allow images from localhost.
+      // Please change this to your domain if you are using a different domain.
+      domains: ["localhost"],
+    },
+    // Note: env variables are set in next.config.js only accept string values so used publicRuntimeConfig instead
+    publicRuntimeConfig: {
+      // Controls posts per page for blog, category and tag pages
+      wordPressDisplaySettings: {
+        postsPerPage: 5,
+      },
+    },
+  };
+  
+  export default nextConfig;
+  
