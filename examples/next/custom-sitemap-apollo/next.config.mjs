23e999688dce2bf699147436e1f58c53a4c3d4a5
ahuseyn <huseyn.aghayev@wpengine.com>
2025-03-27 18:05:56 +0100

    feat: redirect sitemap to sitemap.xml

--- examples/next/custom-sitemap-apollo/next.config.mjs
+++ examples/next/custom-sitemap-apollo/next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: "/sitemap",
+        destination: "/sitemap.xml",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
