82198dd86d62c128af0c27b3d42bc372eb3ff3b8
Theo <328805+theodesp@users.noreply.github.com>
2025-04-03 09:12:59 +0100

    chore: remove unnecessary rewrites config

--- examples/next/hybrid-sitemap-apollo/example-app/next.config.mjs
+++ examples/next/hybrid-sitemap-apollo/example-app/next.config.mjs
@@ -1,14 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  async rewrites() {
-    return [
-      {
-        source: "/:sitemap(wp-sitemap-.*).xml",
-        destination: "/api/sitemap?sitemap=:sitemap",
-      },
-    ];
-  },
 };
 
 export default nextConfig;
