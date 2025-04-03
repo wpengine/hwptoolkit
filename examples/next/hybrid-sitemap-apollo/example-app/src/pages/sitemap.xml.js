d5605aa1909151706df7856aedc90e540754cd6e
Theo <328805+theodesp@users.noreply.github.com>
2025-04-03 09:08:52 +0100

    chore: replace @xmldom/xmldom with fast-xml-parser

--- examples/next/hybrid-sitemap-apollo/example-app/src/pages/sitemap.xml.js
+++ examples/next/hybrid-sitemap-apollo/example-app/src/pages/sitemap.xml.js
@@ -1,17 +1,61 @@
-import { DOMParser, XMLSerializer } from "@xmldom/xmldom";
+import { XMLParser } from 'fast-xml-parser';
 
 const wordpressUrl =
-  process.env.NEXT_PUBLIC_WORDPRESS_URL || "http://localhost:8888";
+  (process.env.NEXT_PUBLIC_WORDPRESS_URL || "http://localhost:8888").trim();
 const frontEndUrl =
-  process.env.NEXT_PUBLIC_FRONTEND_URL || "http://localhost:3000";
+  (process.env.NEXT_PUBLIC_FRONTEND_URL || "http://localhost:3000").trim();
+
+// Parser configuration
+const parserConfig = {
+  ignoreAttributes: false,
+  preserveOrder: false,
+  unpairedTags: ['xml', 'xml-stylesheet'],
+  processEntities: true,
+  htmlEntities: true,
+};
+
+function trimSlashes(str) {
+  return str.replace(/^\/+|\/+$/g, '');
+}
+
+function createSitemap(urls) {
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+<?xml-stylesheet type="text/xsl" href="${frontEndUrl}/sitemap.xsl"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  ${urls.map(url => `
+  <url>
+    <loc>${url.loc}</loc>
+    ${url.lastmod ? `<lastmod>${url.lastmod}</lastmod>` : ''}
+    ${url.changefreq ? `<changefreq>${url.changefreq}</changefreq>` : ''}
+    ${url.priority ? `<priority>${url.priority}</priority>` : ''}
+  </url>`).join('')}
+</urlset>`;
+
+  return sitemap;
+}
+
+function createSitemapIndex(sitemaps) {
+  const sitemapIndex = `<?xml version="1.0" encoding="UTF-8"?>
+<?xml-stylesheet type="text/xsl" href="${frontEndUrl}/sitemap.xsl"?>
+<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  ${sitemaps.map(sitemap => `
+  <sitemap>
+    <loc data-real-url="${sitemap.realUrl || sitemap.loc}">${sitemap.loc}</loc>
+    ${sitemap.lastmod ? `<lastmod>${sitemap.lastmod}</lastmod>` : ''}
+  </sitemap>`).join('')}
+</sitemapindex>`;
+
+  return sitemapIndex;
+}
 
 export async function getServerSideProps({ req, res }) {
   const url = new URL(req.url, `http://${req.headers.host}`);
   const sitemapParam = url.searchParams.get("sitemap");
 
   const wpSitemapUrl = sitemapParam
-    ? `${wordpressUrl.replace(/\/$/, '')}${sitemapParam.startsWith('/') ? sitemapParam : `/${sitemapParam}`}`
-    : `${wordpressUrl.replace(/\/$/, '')}/sitemap.xml`;
+    ? `${trimSlashes(wordpressUrl)}/${sitemapParam.replace(/^\/+/, '')}`
+    : `${trimSlashes(wordpressUrl)}/sitemap.xml`;
+  
   console.debug("Fetching sitemap", wpSitemapUrl);
   const response = await fetch(wpSitemapUrl);
 
@@ -20,66 +64,52 @@ export async function getServerSideProps({ req, res }) {
   }
 
   const xmlText = await response.text();
-  const doc = new DOMParser().parseFromString(xmlText, "text/xml");
+  const isIndex = xmlText.includes("<sitemapindex");
+  const parser = new XMLParser({
+    ...parserConfig,
+    isArray: (tagName) => tagName === (isIndex ? 'sitemap' : 'url'),
+  });
 
-  // Remove the xml-stylesheet node if it exists
-  const nodesToRemove = [];
-  for (let i = 0; i < doc.childNodes.length; i++) {
-    const node = doc.childNodes[i];
-    if (node.nodeType === 7 && node.nodeName === "xml-stylesheet") {
-      nodesToRemove.push(node);
-    }
-  }
-  nodesToRemove.forEach((node) => doc.removeChild(node));
-  const isIndex = doc.getElementsByTagName("sitemapindex").length > 0;
-
-  const newStylesheetPI = doc.createProcessingInstruction(
-    "xml-stylesheet",
-    `type="text/xsl" href="${frontEndUrl}/sitemap.xsl"`
-  );
-  doc.insertBefore(newStylesheetPI, doc.childNodes[0].nextSibling);
+  const parsedXml = parser.parse(xmlText);
 
   if (isIndex) {
-    const sitemaps = doc.getElementsByTagName("sitemap");
-    for (let i = 0; i < sitemaps.length; i++) {
-      const locNode = sitemaps[i].getElementsByTagName("loc")[0];
-      if (locNode) {
-        const wpPath = new URL(locNode.textContent).pathname;
-        // Extract the filename without the path and extension
-        const filenameMatch = wpPath.match(/\/([^\/]+)\.xml$/);
-        if (filenameMatch && filenameMatch[1]) {
-          const displayName = filenameMatch[1];
-          // Store the actual link in a custom attribute for the XSL to use
-          // But display a cleaner URL text
-          locNode.textContent = `${frontEndUrl}/sitemaps/${displayName}`;
-          
-          // We'll add a hidden attribute that the XSL can use to get the real URL
-          const realUrlAttr = doc.createAttribute("data-real-url");
-          realUrlAttr.value = `${frontEndUrl}/sitemap.xml?sitemap=${wpPath}`;
-          locNode.attributes.setNamedItem(realUrlAttr);
-        } else {
-          // Fallback to the old method if the pattern doesn't match
-          locNode.textContent = `${frontEndUrl}/sitemap.xml?sitemap=${wpPath}`;
-        }
-      }
-    }
-  } else {
-    const urls = doc.getElementsByTagName("url");
-    for (let i = 0; i < urls.length; i++) {
-      const locNode = urls[i].getElementsByTagName("loc")[0];
-      if (locNode) {
-        locNode.textContent = locNode.textContent.replace(
-          wordpressUrl,
-          frontEndUrl
-        );
-      }
-    }
-  }
+    const wpSitemaps = parsedXml?.sitemapindex?.sitemap;
+    if (!wpSitemaps) return { notFound: true };
 
-  const updatedXml = new XMLSerializer().serializeToString(doc);
-  res.setHeader("Content-Type", "application/xml");
-  res.write(updatedXml);
-  res.end();
+    const sitemaps = wpSitemaps.map(sitemap => {
+      const url = new URL(sitemap.loc);
+      const wpPath = url.pathname;
+      const filenameMatch = wpPath.match(/\/([^\/]+)\.xml$/);
+      
+      let displayLoc = filenameMatch && filenameMatch[1]
+        ? `${trimSlashes(frontEndUrl)}/sitemaps/${filenameMatch[1]}`
+        : `${trimSlashes(frontEndUrl)}/sitemap.xml?sitemap=${wpPath}`;
+
+      return {
+        ...sitemap,
+        loc: displayLoc,
+        realUrl: `${trimSlashes(frontEndUrl)}/sitemap.xml?sitemap=${wpPath}`
+      };
+    });
+
+    const updatedXml = createSitemapIndex(sitemaps);
+    res.setHeader("Content-Type", "application/xml");
+    res.write(updatedXml);
+    res.end();
+  } else {
+    const wpUrls = parsedXml?.urlset?.url;
+    if (!wpUrls) return { notFound: true };
+
+    const urls = wpUrls.map(url => ({
+      ...url,
+      loc: url.loc.replace(trimSlashes(wordpressUrl), trimSlashes(frontEndUrl))
+    }));
+
+    const updatedXml = createSitemap(urls);
+    res.setHeader("Content-Type", "application/xml");
+    res.write(updatedXml);
+    res.end();
+  }
 
   return { props: {} };
 }
