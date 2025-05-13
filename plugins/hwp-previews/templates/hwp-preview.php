cb18a5c7bb200dd17ec2a3702d869789f124cc1a
ahuseyn <huseyn.aghayev@wpengine.com>
2025-05-13 14:05:33 +0200

    fix: linting issues

--- plugins/hwp-previews/templates/hwp-preview.php
+++ plugins/hwp-previews/templates/hwp-preview.php
@@ -3,34 +3,38 @@
  * Custom Preview Template
  *
  * This template will be used for all post/page previews
+ *
+ * @package HWP\Previews
  */
+
+declare(strict_types=1);
 
 use HWP\Previews\Preview\Template\Preview_Template_Resolver;
 
-$preview_url = (string) get_query_var( Preview_Template_Resolver::HWP_PREVIEWS_IFRAME_PREVIEW_URL );
+$hwp_previews_url_template = (string) get_query_var( Preview_Template_Resolver::HWP_PREVIEWS_IFRAME_PREVIEW_URL );
 
 ?>
 
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
-    <head>
-        <?php wp_head(); ?>
-    </head>
+	<head>
+		<?php wp_head(); ?>
+	</head>
 
-    <body style="overflow: hidden;">
-        <?php wp_body_open(); ?>
+	<body style="overflow: hidden;">
+		<?php wp_body_open(); ?>
 
-            <div style="position: relative;">
-                <iframe
-                        src="<?php echo esc_url( $preview_url ); ?>"
-                        class="headless-preview-frame"
-                        sandbox="allow-scripts allow-same-origin allow-forms"
-                        referrerpolicy="no-referrer-when-downgrade"
-                        title="Content Preview"
-                        style="width: 100%; height: calc(100vh - var(--wp-admin--admin-bar--height)); border: none;">
-                </iframe>
-            </div>
+			<div style="position: relative;">
+				<iframe
+						src="<?php echo esc_url( $hwp_previews_url_template ); ?>"
+						class="headless-preview-frame"
+						sandbox="allow-scripts allow-same-origin allow-forms"
+						referrerpolicy="no-referrer-when-downgrade"
+						title="Content Preview"
+						style="width: 100%; height: calc(100vh - var(--wp-admin--admin-bar--height)); border: none;">
+				</iframe>
+			</div>
 
-        <?php wp_footer(); ?>
-    </body>
+		<?php wp_footer(); ?>
+	</body>
 </html>
