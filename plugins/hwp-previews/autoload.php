a149a39dc2c203081b73b9828018da837e1ed15e
Alex Kazhukhouski <alex.kazhukhouski@wpengine.com>
2025-05-15 13:48:29 +0200

    Updates autoload file. Includes it into phpcs, phpstan and psalm configs.

--- plugins/hwp-previews/autoload.php
+++ plugins/hwp-previews/autoload.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * Autoloader for HWP Previews Plugin.
  *
@@ -9,42 +8,45 @@
  * @package HWP\Previews
  */
 
+declare(strict_types=1);
+
 if ( file_exists( __DIR__ . '/vendor/autoload.php' ) ) {
 	require __DIR__ . '/vendor/autoload.php';
 
 	return;
 }
 
-spl_autoload_register( function ( $class ) {
-	// Define namespace prefix for your plugin
+spl_autoload_register( static function ( $hwp_class ) {
+	// Define namespace prefix for your plugin.
 	$prefix   = 'HWP\\Previews\\';
 	$base_dir = __DIR__ . '/src/';
 
 	$len = strlen( $prefix );
-	if ( strncmp( $prefix, $class, $len ) !== 0 ) {
+	if ( strncmp( $prefix, $hwp_class, $len ) !== 0 ) {
 		return;
 	}
 
-	$file = $base_dir . str_replace( '\\', '/', substr( $class, $len ) ) . '.php';
+	$hwp_file = $base_dir . str_replace( '\\', '/', substr( $hwp_class, $len ) ) . '.php';
 
-	if ( file_exists( $file ) ) {
-		require $file;
+	if ( file_exists( $hwp_file ) ) {
+		// phpcs:ignore WordPressVIPMinimum.Files.IncludingFile.UsingVariable
+		require $hwp_file;
 
 		return;
 	}
 
-	if ( ! defined( 'WP_DEBUG' ) || ! WP_DEBUG ) {
-		_doing_it_wrong(
-			__FUNCTION__,
-			sprintf(
-				/* translators: 1: Class name, 2: File path */
-				__( 'Class %1$s could not be loaded. File %2$s not found.', 'hwp-previews' ),
-				'<code>' . esc_html( $class ) . '</code>',
-				'<code>' . esc_html( $file ) . '</code>'
-			),
-			'1.0.0'
-		);
-
-		error_log( sprintf( 'HWP Previews: Failed to load class %s, file %s not found', $class, $file ) );
+	if ( defined( 'WP_DEBUG' ) && WP_DEBUG ) {
+		return;
 	}
+
+	_doing_it_wrong(
+		__FUNCTION__,
+		sprintf(
+			/* translators: 1: Class name, 2: File path */
+			esc_html( __( 'Class %1$s could not be loaded. File %2$s not found.', 'hwp-previews' ) ),
+			'<code>' . esc_html( $hwp_class ) . '</code>',
+			'<code>' . esc_html( $hwp_file ) . '</code>'
+		),
+		'1.0.0'
+	);
 } );
