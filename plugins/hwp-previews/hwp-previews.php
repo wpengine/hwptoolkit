816b78c50b60b4587e94aa40047aa61425ddb7ea
Colin Murphy <colinmurphy@users.noreply.github.com>
2025-05-23 11:25:34 +0100

    Various changes for plugin initialisation and  updated/added some missing files for plugin

--- plugins/hwp-previews/hwp-previews.php
+++ plugins/hwp-previews/hwp-previews.php
@@ -1,17 +1,20 @@
 <?php
 /**
  * Plugin Name: HWP Previews
+ * Plugin URI: https://github.com/wpengine/hwptoolkit
+ * GitHub Plugin URI: https://github.com/wpengine/hwptoolkit
  * Description: Headless Previews solution as a WordPress plugin with extensive configurability.
- * Author: WP Engine
- * Author URI: https://wpengine.com/
- * License: GPLv2 or later
- * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * Author: WPEngine Headless OSS Team
+ * Author URI: https://github.com/wpengine
+ * Update URI: https://github.com/wpengine/hwptoolkit
+ * Version: 0.0.1
  * Text Domain: hwp-previews
  * Domain Path: /languages
- * Tested up to: 6.8.1
- * Version: 0.0.1
- * Requires PHP: 7.4
  * Requires at least: 6.0
+ * Tested up to: 6.8
+ * Requires PHP: 7.4+
+ * License: GPLv2 or later
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
  *
  * @package HWP\Previews
  */
@@ -22,17 +25,85 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-require __DIR__ . '/autoload.php';
+// Load the autoloader.
+require_once __DIR__ . '/src/Autoloader.php';
+if ( ! \HWP\Previews\Autoloader::autoload() ) {
+	return;
+}
 
-define( 'HWP_PREVIEWS_BASENAME', plugin_basename( __FILE__ ) );
+// @TODO: https://github.com/wpengine/hwptoolkit/issues/183
+if ( file_exists( __DIR__ . '/activation.php' ) ) {
+	require_once __DIR__ . '/activation.php';
+	register_activation_hook( __FILE__, 'hwp_previews_activation_callback' );
+}
 
-add_action(
-	'plugins_loaded',
-	static fn () => HWP\Previews\Plugin::get_instance(
-		'0.0.1',
-		plugin_dir_path( __FILE__ ),
-		plugin_dir_url( __FILE__ )
-	)->init(),
-	5,
-	0
-);
+if ( file_exists( __DIR__ . '/deactivation.php' ) ) {
+	require_once __DIR__ . '/deactivation.php';
+	register_deactivation_hook( __FILE__, 'hwp_previews_deactivation_callback' );
+}
+
+/**
+ * Define plugin constants.
+ */
+function hwp_previews_constants(): void {
+	// Plugin version.
+	if ( ! defined( 'HWP_PREVIEWS_VERSION' ) ) {
+		define( 'HWP_PREVIEWS_VERSION', '0.0.1' );
+	}
+
+	// Plugin Folder Path.
+	if ( ! defined( 'HWP_PREVIEWS_PLUGIN_DIR' ) ) {
+		define( 'HWP_PREVIEWS_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
+	}
+
+	// Plugin Folder URL.
+	if ( ! defined( 'HWP_PREVIEWS_PLUGIN_URL' ) ) {
+		define( 'HWP_PREVIEWS_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
+	}
+
+	// Plugin Root File.
+	if ( ! defined( 'HWP_PREVIEWS_PLUGIN_FILE' ) ) {
+		define( 'HWP_PREVIEWS_PLUGIN_FILE', __FILE__ );
+	}
+
+	// Whether to autoload the files or not.
+	if ( ! defined( 'HWP_PREVIEWS_AUTOLOAD' ) ) {
+		define( 'HWP_PREVIEWS_AUTOLOAD', true );
+	}
+}
+
+/**
+ * Initializes plugin.
+ */
+function hwp_previews_init() : void {
+	hwp_previews_constants();
+
+
+	if ( defined( 'HWP_PREVIEWS_PLUGIN_DIR' ) ) {
+		require_once HWP_PREVIEWS_PLUGIN_DIR . 'src/Plugin.php';
+		\HWP\Previews\Plugin::instance();
+		return;
+	}
+
+
+	add_action(
+		'admin_notices',
+		static function () {
+			?>
+			<div class="error notice">
+				<p>
+					<?php
+					printf(
+						esc_html__( 'Composer vendor directory must be present for HWP Previews to work.', 'hwp-previews' ),
+					);
+					?>
+				</p>
+			</div>
+			<?php
+		},
+		10,
+		0
+	);
+}
+/** @psalm-suppress HookNotFound */
+add_action( 'plugins_loaded', 'hwp_previews_init', 15 );
--- plugins/hwp-previews/readme.txt
+++ plugins/hwp-previews/readme.txt
@@ -1,16 +1,18 @@
 === WPGraphQL Plugin Name ===
 Contributors: wpengine
-Tags: GraphQL, Gatsby, Headless, WPGraphQL, React, Rest, Webhooks
+Tags: GraphQL, Headless, Previews, WPGraphQL, React, Rest
 Requires at least: 6.0
-Tested up to: 6.8
+Tested up to: 6.8.1
 Requires PHP: 7.4
-Requires WPGraphQL: 1.8.0
 Stable tag: 0.0.1
 License: GPL-3
 License URI: https://www.gnu.org/licenses/gpl-3.0.html
 
 == Description ==
-== Upgrade Notice == 
+
+A headless WordPress plugin for Previews.
+
+== Upgrade Notice ==
 == Frequently Asked Questions ==
 == Screenshots ==
 == Changelog ==
