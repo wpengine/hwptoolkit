a437ef86142626c9c0f60eda5decc273fa13d3a8
Colin Murphy <colinmurphy@users.noreply.github.com>
2025-05-23 18:15:24 +0100

    Update deactivation.php

--- plugins/wp-graphql-headless-webhooks/deactivation.php
+++ plugins/wp-graphql-headless-webhooks/deactivation.php
@@ -9,13 +9,12 @@ declare(strict_types=1);
  */
 
 /**
- * Runs when WPGraphQL is de-activated.
+ * Runs when WPGraphQL Headless Webhooks is de-activated.
  *
- * This cleans up data that WPGraphQL stores.
  */
 function graphql_headless_webhooks_deactivation_callback(): callable {
 	return static function (): void {
-		// Fire an action when WPGraphQL is de-activating.
+		// Fire an action when WPGraphQL Headless Webhook is de-activating.
 		do_action( 'graphql_webhooks_deactivate' );
 	};
 }
