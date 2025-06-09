39b8e5ef05bde1b634040ddcd0991ae05b5f2ad8
Joe Fusco <hello@josephfus.co>
2025-06-09 16:22:28 -0400

    Consolidate clean up scripts

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -1,21 +1,10 @@
 #!/bin/bash
 
-echo "Stopping wp-env containers..."
-docker-compose -f ~/.wp-env/*/docker-compose.yml down 2>/dev/null || true
-
-echo "Removing wp-env containers..."
+# Stop and remove all Docker containers with wp-env prefix
 docker ps -a -q -f name=wp-env | xargs -r docker rm -f
 
-echo "Removing wp-env volumes..."
-docker volume ls -q -f name=wp-env | xargs -r docker volume rm -f
+# Remove wp-env state directories
+rm -rf ~/.wp-env
 
-echo "Removing WordPress volumes..."
-docker volume ls -q -f name=wordpress | xargs -r docker volume rm -f
-
-echo "Removing all test volumes..."
-docker volume ls -q -f name=tests-wordpress | xargs -r docker volume rm -f
-
-echo "Cleaning up networks..."
+# Prune Docker networks
 docker network prune -f
-
-echo "Cleanup complete! You may need to wait a few seconds before starting wp-env again."
