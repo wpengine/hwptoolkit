73689326a2e509d46ec02ab51babc38a56033653
Colin Murphy <colinmurphy@users.noreply.github.com>
2025-07-02 13:33:11 +0100

    Removed comments to force an update for previews to test new workflow change.

--- plugins/hwp-previews/tests/bootstrap.php
+++ plugins/hwp-previews/tests/bootstrap.php
@@ -7,17 +7,14 @@
  * @link https://github.com/wpengine/hwptoolkit/blob/main/plugins/hwp-previews/TESTING.md
  */
 
-// Ensure proper autoloading
 if (file_exists(__DIR__ . '/../../vendor/autoload.php')) {
 	require_once __DIR__ . '/../../vendor/autoload.php';
 }
 
-// Load the main plugin file to ensure all classes are available
 if (file_exists(__DIR__ . '/../../hwp-previews.php')) {
 	require_once __DIR__ . '/../../hwp-previews.php';
 }
 
-// Load access functions if they exist
 if (file_exists(__DIR__ . '/../../access-functions.php')) {
 	require_once __DIR__ . '/../../access-functions.php';
 }
