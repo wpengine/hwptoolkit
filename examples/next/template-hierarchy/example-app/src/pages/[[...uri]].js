1afc501bffc633f44683e743757a583d105e4f8e
Alex Moon <moonmeister@users.noreply.github.com>
2025-07-31 15:02:32 -0700

    feat: implement data fetching for templates

--- examples/next/template-hierarchy/example-app/src/pages/[[...uri]].js
+++ examples/next/template-hierarchy/example-app/src/pages/[[...uri]].js
@@ -1,6 +1,7 @@
 import { uriToTemplate } from "@/lib/templateHierarchy";
 import { RouteDataProvider } from "@/lib/context";
 import availableTemplates from "@/wp-templates";
+import { fetchQueries } from "@/lib/queryHandler";
 
 export default function Page(props) {
   const { templateData } = props;
@@ -14,7 +15,8 @@ export default function Page(props) {
   );
 }
 
-export async function getServerSideProps({ params }) {
+export async function getServerSideProps(context) {
+  const { params } = context;
   const uri = Array.isArray(params.uri)
     ? "/" + params.uri?.join("/") + "/"
     : "/";
@@ -30,12 +32,25 @@ export async function getServerSideProps({ params }) {
     };
   }
 
+  const PageTemplate = availableTemplates[templateData.template?.id];
+
+  const component = await PageTemplate.render.preload();
+
+  const graphqlData = await fetchQueries({
+    queries: component.default.queries,
+    context,
+    props: {
+      uri,
+      templateData,
+    },
+  });
+
   return {
     props: {
-      params,
       uri,
       // https://github.com/vercel/next.js/discussions/11209#discussioncomment-35915
       templateData: JSON.parse(JSON.stringify(templateData)),
+      graphqlData: JSON.parse(JSON.stringify(graphqlData)),
     },
   };
 }
