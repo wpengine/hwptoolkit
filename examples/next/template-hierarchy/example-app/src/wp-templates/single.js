2c31dd3df8b164f0ea55c7318513fbf1cbea5e9f
Alex Moon <moonmeister@users.noreply.github.com>
2025-07-31 15:49:11 -0700

    feat: add data fetching to

--- examples/next/template-hierarchy/example-app/src/wp-templates/single.js
+++ examples/next/template-hierarchy/example-app/src/wp-templates/single.js
@@ -1,10 +1,36 @@
 import Layout from "@/components/Layout";
 
-export default function SingleTemplate() {
+export default function SingleTemplate({ graphqlData }) {
+  const { SinglePostQuery } = graphqlData;
   return (
     <Layout>
-      <h2>Single Template</h2>
-      <p>This is the single template for the template hierarchy example app.</p>
+      <h2>{SinglePostQuery.response.data.post.title}</h2>
+      <div
+        dangerouslySetInnerHTML={{
+          __html: SinglePostQuery.response.data.post.content,
+        }}
+      />
     </Layout>
   );
 }
+
+export const queries = "test";
+
+SingleTemplate.queries = [
+  {
+    name: "SinglePostQuery",
+    query: /* GraphQL */ `
+      query SinglePostQuery($id: ID!) {
+        post(id: $id, idType: URI) {
+          id
+          title
+          content
+          date
+        }
+      }
+    `,
+    variables: (event, { uri }) => ({
+      id: uri,
+    }),
+  },
+];
