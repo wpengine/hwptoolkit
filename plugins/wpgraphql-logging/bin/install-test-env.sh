4ce8dcaff0519158be2d3fd7cfa84efd8a1242aa
Colin Murphy <colinmurphy@users.noreply.github.com>
2025-08-20 18:18:25 +0100

    Disable SSL mode for MySQL commands in test env script
    Adds --ssl-mode=DISABLED to mysql and mysqladmin commands in install-test-env.sh to prevent SSL connection issues during database setup.

--- plugins/wpgraphql-logging/bin/install-test-env.sh
+++ plugins/wpgraphql-logging/bin/install-test-env.sh
@@ -44,9 +44,9 @@ install_db() {
 	# create database
 	echo -e "$(status_message "Creating the database (if it does not exist)...")"
 
-	RESULT=$(mysql -u $WORDPRESS_DB_USER --password="$WORDPRESS_DB_PASSWORD" --skip-column-names -e "SHOW DATABASES LIKE '$WORDPRESS_DB_NAME'"$EXTRA)
+	RESULT=$(mysql --ssl-mode=DISABLED -u $WORDPRESS_DB_USER --password="$WORDPRESS_DB_PASSWORD" --skip-column-names -e "SHOW DATABASES LIKE '$WORDPRESS_DB_NAME'"$EXTRA)
 	if [ "$RESULT" != $WORDPRESS_DB_NAME ]; then
-		mysqladmin create $WORDPRESS_DB_NAME --user="$WORDPRESS_DB_USER" --password="$WORDPRESS_DB_PASSWORD"$EXTRA
+		mysqladmin --ssl-mode=DISABLED create $WORDPRESS_DB_NAME --user="$WORDPRESS_DB_USER" --password="$WORDPRESS_DB_PASSWORD"$EXTRA
 	fi
 }
 
