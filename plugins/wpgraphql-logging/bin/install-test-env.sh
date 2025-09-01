37d06dd75cec83d4cdda209084c3a1b048bee36d
Colin Murphy <colinmurphy@users.noreply.github.com>
2025-09-01 17:48:02 +0100

    Fixing SSL flag.

--- plugins/wpgraphql-logging/bin/install-test-env.sh
+++ plugins/wpgraphql-logging/bin/install-test-env.sh
@@ -44,9 +44,9 @@ install_db() {
 	# create database
 	echo -e "$(status_message "Creating the database (if it does not exist)...")"
 
-	RESULT=$(mysql -u $WORDPRESS_DB_USER --password="$WORDPRESS_DB_PASSWORD" --skip-column-names --ssl-mode=DISABLED -e "SHOW DATABASES LIKE '$WORDPRESS_DB_NAME'"$EXTRA)
+	RESULT=$(mysql -u $WORDPRESS_DB_USER --password="$WORDPRESS_DB_PASSWORD" --skip-column-names --ssl=0 -e "SHOW DATABASES LIKE '$WORDPRESS_DB_NAME'"$EXTRA)
 	if [ "$RESULT" != $WORDPRESS_DB_NAME ]; then
-		mysqladmin create $WORDPRESS_DB_NAME --user="$WORDPRESS_DB_USER" --password="$WORDPRESS_DB_PASSWORD" --ssl-mode=DISABLED$EXTRA
+		mysqladmin create $WORDPRESS_DB_NAME --user="$WORDPRESS_DB_USER" --password="$WORDPRESS_DB_PASSWORD" --ssl=0$EXTRA
 	fi
 }
 
