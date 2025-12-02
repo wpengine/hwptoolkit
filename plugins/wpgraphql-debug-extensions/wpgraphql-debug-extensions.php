84b3a84191c4392ab5f84409530a9d9be7c39b90
Joe Fusco <hello@josephfus.co>
2025-12-02 15:20:46 -0500

    chore: test wpgraphql-debug-extensions against WordPress 6.9
    - Update "Tested up to" to 6.9 in readme.txt and plugin header
    - Fix phpcompatibility/php-compatibility from dev-develop to ^9.3
    - Update CI matrix: add WP 6.9, remove 6.5/6.6, make 6.9 default

--- plugins/wpgraphql-debug-extensions/wpgraphql-debug-extensions.php
+++ plugins/wpgraphql-debug-extensions/wpgraphql-debug-extensions.php
@@ -11,7 +11,7 @@
  * Text Domain: wpgraphql-debug-extensions
  * Domain Path: /languages
  * Requires at least: 6.0
- * Tested up to: 6.8
+ * Tested up to: 6.9
  * Requires PHP: 7.4+
  * Requires Plugins: wp-graphql
  * WPGraphQL requires at least: 1.8.0
